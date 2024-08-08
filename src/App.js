6c0900d36ae123e58ca77ba30b7abc5ca014e3d3
ilving Florentino <ilvingflorentino2@gmail.com>
2024-08-08 09:58:59 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import imagenrich from "./img/rich.png";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  //!estado de State characters inicia en null asta que la api le pasa el json y luego ese null en este caso se convierte en results que es lo que quiero de mi Api.
+  const [characters, setCharacters] = useState(null);
+
+  //? funcion de Clik funcion asyncrona que siempre debe tener await.
+  const reqApi = async () => {
+    const api = await fetch("https://rickandmortyapi.com/api/character");
+    //?lo que resibe el state del json de la api es el Results
+    const { results } = await api.json();
+    //? Aqui el characters deja de ser null coje el json y toma todos los resultados
+    setCharacters(results);
+  };
+
+  console.log(characters);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="title">Rich y Morty</h1>
+
+        <>
+          {" "}
+          <img src={imagenrich} alt="Rich & Morty" className="img-home"></img>
+          <button onClick={reqApi} className="btn-search">
+            Buscar personajes
+          </button>
+        </>
       </header>
     </div>
   );
---
+++ src/components/Characters.js
@@ -0,0 +1,7 @@
+export default function Characters() {
+  return (
+    <div>
+      <h2>Qloq desde componets Characters.</h2>
+    </div>
+  );
+}
