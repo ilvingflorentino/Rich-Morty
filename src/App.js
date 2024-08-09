5e058aa50660ef58ff06f350b540b8d9319f2ec7
ilving Florentino <ilvingflorentino2@gmail.com>
2024-08-09 11:17:52 -0400

    practicando el react.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import imagenrich from "./img/rich.png";
 import "./App.css";
 import { useState } from "react";
+import Characters from "./components/Characters";
 
 function App() {
   //!estado de State characters inicia en null asta que la api le pasa el json y luego ese null en este caso se convierte en results que es lo que quiero de mi Api.
@@ -14,21 +15,21 @@ function App() {
     //? Aqui el characters deja de ser null coje el json y toma todos los resultados
     setCharacters(results);
   };
-
-  console.log(characters);
-
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="title">Rich y Morty</h1>
-
-        <>
-          {" "}
-          <img src={imagenrich} alt="Rich & Morty" className="img-home"></img>
-          <button onClick={reqApi} className="btn-search">
-            Buscar personajes
-          </button>
-        </>
+        {characters ? (
+          <Characters characters={characters} setCharacters={setCharacters}/>
+        ) : (
+          <>
+            {" "}
+            <img src={imagenrich} alt="Rich & Morty" className="img-home"></img>
+            <button onClick={reqApi} className="btn-search">
+              Buscar personajes
+            </button>
+          </>
+        )}
       </header>
     </div>
   );
--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,7 +1,49 @@
-export default function Characters() {
+export default function Characters(props) {
+  const { characters, setCharacters } = props;
+
+  const resetCharacters = () => {
+    setCharacters(null);
+  };
   return (
-    <div>
-      <h2>Qloq desde componets Characters.</h2>
+    <div className="characters">
+      <span className="back-home" onClick={resetCharacters}>
+        Volver al Home
+      </span>
+      <h1>Personajes</h1>
+      <div className="container-characters">
+        {characters.map((character, index) => (
+          <div className="character-container" key={index}>
+            <div>
+              <img src={character.image} alt={character.name} />
+            </div>
+            <div>
+              <h3>{character.name}</h3>
+              <h6>
+                {character.status === "Alive" ? (
+                  <>
+                    <span className="alive"> Alive </span>
+                  </>
+                ) : (
+                  <>
+                    <span className="dead"> Dead</span>
+                  </>
+                )}
+              </h6>
+              <p>
+                <span className="text-grey">Episodios: </span>
+                <span>{character.episode.length}</span>
+              </p>
+              <p>
+                <span className="text-grey">Especie:</span>
+                <span>{character.species}</span>
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <span className="back-home" onClick={resetCharacters}>
+        Volver a la home
+      </span>
     </div>
   );
 }
